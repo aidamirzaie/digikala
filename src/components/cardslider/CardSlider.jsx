3ffee3f36c8f7e7aee52454ec3545d0baf4506a6
aida Mirzaie <aydamirzaie1376@gmail.com>
2025-05-01 16:15:03 +0330

    refactor(component): formate code with prettier

--- src/components/cardslider/CardSlider.jsx
+++ src/components/cardslider/CardSlider.jsx
@@ -1,21 +1,27 @@
 import ProductCard from "../productcard/ProductCard";
-import './cardslider.css'
-import mobileimg from '../../assets/mobile.webp'
-import headimg from '../../assets/headset.webp'
-function CardSlider()
-{
-    return(
-        <>
-            <diV className="slider">   
-                <ProductCard title="phone" discount="4,000,000 Rial" discount_percentage="40%" price="8,000,000 Rial" img={mobileimg} />
-                <ProductCard title="hedphone"discount="6,000,000 Rial"  discount_percentage="30%" price="7,500,00 Rial" img={headimg}/>
- 
-            </diV>
-     
-
-        </>
-    );
-
-
+import "./cardslider.css";
+import mobileimg from "../../assets/mobile.webp";
+import headimg from "../../assets/headset.webp";
+function CardSlider() {
+  return (
+    <>
+      <diV className="slider">
+        <ProductCard
+          title="phone"
+          discount="4,000,000 Rial"
+          discount_percentage="40%"
+          price="8,000,000 Rial"
+          img={mobileimg}
+        />
+        <ProductCard
+          title="hedphone"
+          discount="6,000,000 Rial"
+          discount_percentage="30%"
+          price="7,500,00 Rial"
+          img={headimg}
+        />
+      </diV>
+    </>
+  );
 }
 export default CardSlider;
