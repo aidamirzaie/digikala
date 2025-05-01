ab32edddf476fc88fa055e4da8f8b2402f7adbbc
aida Mirzaie <aydamirzaie1376@gmail.com>
2025-05-01 16:29:29 +0330

    feat(component): add child to cardslider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import CardSlider from './components/cardslider/CardSlider'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CardSlider from "./components/cardslider/CardSlider";
+import ProductCard from "./components/productcard/ProductCard";
+import mobileimg from "./assets/mobile.webp";
+import headimg from './assets/headset.webp'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>digikala</h1>
-     <CardSlider/>
+      <h1>digikala</h1>
+      <CardSlider>
+        <ProductCard
+          title="phone"
+          discount="4,000,000 Rial"
+          discount_percentage="40%"
+          price="8,000,000 Rial"
+          img={mobileimg}
+        />
+        <ProductCard
+          title="hedphone"
+          discount="6,000,000 Rial"
+          discount_percentage="30%"
+          price="7,500,00 Rial"
+          img={headimg}
+        />
+      </CardSlider>
+      <div>
+        <p></p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/cardslider/CardSlider.jsx
+++ src/components/cardslider/CardSlider.jsx
@@ -1,25 +1,12 @@
 import ProductCard from "../productcard/ProductCard";
 import "./cardslider.css";
-import mobileimg from "../../assets/mobile.webp";
-import headimg from "../../assets/headset.webp";
-function CardSlider() {
+
+function CardSlider(props) {
   return (
     <>
       <diV className="slider">
-        <ProductCard
-          title="phone"
-          discount="4,000,000 Rial"
-          discount_percentage="40%"
-          price="8,000,000 Rial"
-          img={mobileimg}
-        />
-        <ProductCard
-          title="hedphone"
-          discount="6,000,000 Rial"
-          discount_percentage="30%"
-          price="7,500,00 Rial"
-          img={headimg}
-        />
+        {props.children}
+
       </diV>
     </>
   );
